43e2f530eddd2fe37d0ef4715555e7b161df26be
UAWeiguoJiang <weiguo@ualberta.ca>
2022-03-28 16:04:38 -0600

    Update tsv-2-json.py

--- tsv-2-json.py
+++ tsv-2-json.py
@@ -1,40 +1,51 @@
 import json
 
+# function adapted from https://www.geeksforgeeks.org/python-tsv-conversion-to-json/
 def tsv2json(input_file,output_file):
-    arr = []
-    file = open(input_file, 'r')
+    """
+        Convert tsv files to json files.
+
+        Arguments:
+            input_file: path to the tsv file
+            output_file: path to the output json file
+
+        Returns: None
+    """
+    arr = []    # create a list for storing all dictionaries
+
+    file = open(input_file, 'r', encoding='utf-8')  # open file and retrieve field names
     a = file.readline()
-    # The first line consist of headings of the record 
-    # so we will store it in an array and move to 
-    # next line in input_file.
     titles = [t.strip() for t in a.split('\t')]
+
     for line in file:
         d = {}
-        for t, f in zip(titles, line.split('\t')):
-            # Convert each row into dictionary with keys as titles
-            f = f.strip()
+        for t, f in zip(titles, line.split('\t')):  # pair up field names and values
+
+            f = f.strip()   # strip extra spaces and newlines
             f = f.strip('\\N')
-            # if f == '\n' or '': will make null in json into '' / empty string
-            if f == '':
+
+            if f == '':     # if empty string, convert to None / null in json
                 d[t] = None
-            elif t == 'primaryProfession' or t == 'knownForTitles' or t == 'genres':
+            elif t == 'primaryProfession' or t == 'knownForTitles' or t == 'genres':    # convert these fields to nested arrays
                 d[t] = f.split(',')
-            elif t == 'characters':
+            elif t == 'characters':     # convert to nested arrays with extra string manipulation
                 l = list()
                 for i in f.strip('][').split(','):
                     l.append(i.strip('\"\"'))
                 d[t] = l
-            elif t == 'runtimeMinutes' or t == 'ordering' or t == 'numVotes':
+            elif t == 'runtimeMinutes' or t == 'ordering' or t == 'numVotes':   # convert these fields to integers
                 d[t] = int(f)
-            elif t == 'averageRating':
+            elif t == 'averageRating':      # convert this field to floats
                 d[t] = float(f)
             else:
                 d[t] = f
 
-        arr.append(d)
+        arr.append(d)   # add dictionary to list
     
-    with open(output_file, 'w', encoding='utf-8') as op:
+    with open(output_file, 'w', encoding='utf-8') as op:    # dump to json file
         op.write(json.dumps(arr, indent=4))
+
+    return
               
         
 
@@ -44,6 +55,8 @@ def main():
     tsv2json('title.basics.tsv', 'title.basics.json')
     tsv2json('title.principals.tsv', 'title.principals.json')
     tsv2json('title.ratings.tsv', 'title.ratings.json')
+
+    return
 
 
 if __name__ == '__main__':
