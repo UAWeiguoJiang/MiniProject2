06e0b7dd3bfc1f1f18d45dcbaf14f706de164147
UAWeiguoJiang <weiguo@ualberta.ca>
2022-03-28 16:08:59 -0600

    Update load-json.py

--- load-json.py
+++ load-json.py
@@ -1,16 +1,25 @@
 import json
 from pymongo import MongoClient
 
-port_number = int(input('Server port number: '))
-client = MongoClient("localhost", port_number)
-db = client['291db']
+port_number = int(input('Server port number: '))    # prompt asking for port number
 
-name_basics = db['name_basics']
+client = MongoClient("localhost", port_number)      # connect to server
+
+db = client['291db']    # open database
+
+name_basics = db['name_basics']     # open or create collections
 title_basics = db['title_basics']
 title_principals = db['title_principals']
 title_ratings = db['title_ratings']
 
 def drop_collections():
+    """
+        Drop previous collections.
+
+        Argument: None
+
+        Returns: None
+    """
     name_basics.drop()
     title_basics.drop()
     title_principals.drop()
@@ -20,6 +29,13 @@ def drop_collections():
 
 
 def create_collections():
+    """
+        Create collections after dropping previous ones.
+
+        Arguments: None
+
+        Returns: None
+    """
     drop_collections()
 
     json2mongo('name.basics.json', name_basics)
@@ -31,15 +47,28 @@ def create_collections():
 
 
 def json2mongo(input_file, collection):
-    with open(input_file, 'r') as f:
-        data = json.load(f)
-        collection.insert_many(data)
+    """
+        Insert into collections.
+
+        Arguments:
+            input_file: path to json file
+            collection: collection name
+
+        Returns: None
+    """
+    with open(input_file, 'r', encoding='utf-8') as f:    # open json file
+
+        data = json.load(f)     # load data from json file
+
+        collection.insert_many(data)    # insert data into the collection
 
     return
 
 
 def main():
     create_collections()
+
+    return
 
 if __name__ == "__main__":
     main()
